0fbc1b93b7a757257e549003a77d35de00c12be2
PetrKochetkov <s22e_kochetkov@179.ru>
2023-02-27 20:21:59 +0300

    Dima lox

---
+++ Tasks/Zadacha23.py
@@ -0,0 +1,22 @@
+def algorith(row):
+    try:
+        a = list(map(int, row['Massive'].split(' ')))
+        mas_even = []
+        for k in range(len(a)):
+            if (lambda x: x % 2 == 0)(a[k]):
+                mas_even.append(k)
+        row['Indexes'] = mas_even
+    except (ValueError, RuntimeError):
+        row['Massive'] = 'Bad input'
+        row['Indexes'] = 'No result'
+    return row
+
+
+def set_variables(current_window):
+    current_window.root.title("Приложение 'Базы данных' (задача 21)")
+    current_window.children.input_window_name = 'Ввод данных для задачи 21'
+    current_window.variables.algorithm = algorith
+    current_window.variables.data_format = [
+        ('Massive', 'TEXT', 'Введите список'),
+        ('Indexes', 'TEXT', 0),
+    ]
